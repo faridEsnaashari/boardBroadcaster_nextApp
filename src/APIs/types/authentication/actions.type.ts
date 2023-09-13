190225acbff3ce0512cb3160e78034f3c1736f82
faridEsnaashari <faride13578642@gmail.com>
2023-09-13 19:09:30 +0330

    SignUpAction and SignUpActionData types added

--- src/APIs/types/authentication/actions.type.ts
+++ src/APIs/types/authentication/actions.type.ts
@@ -1,9 +1,23 @@
 import { StatusCodes } from "@/tools/status-codes.tools";
-import { VerifyEmailActionTypes } from "./authentication.enum";
+import {
+  SignUpActionTypes,
+  VerifyEmailActionTypes,
+} from "./authentication.enum";
 
 export type VerifyEmailActionData = string;
 
 export type VerifyEmailAction = {
   statusCode?: StatusCodes;
   type: VerifyEmailActionTypes;
+};
+
+export type SignUpActionData = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type SignUpAction = {
+  statusCode?: StatusCodes;
+  type: SignUpActionTypes;
 };
