c0aad6fef389d0fa2bf453a3f7f4a25d05a46730
faridEsnaashari <faride13578642@gmail.com>
2023-09-13 13:20:52 +0330

    verifyEmail reducer

---
+++ src/APIs/reducers/authentication/sign-up.reducer.ts
@@ -0,0 +1,47 @@
+import {
+  FailedVerifyEmail,
+  RecivedVerifyEmail,
+  VerifyEmailAction,
+} from "@/APIs/types/authentication/actions.type";
+import { VerifyEmailActionTypes } from "@/APIs/types/authentication/authentication.enum";
+import { VerifyEmailState } from "@/APIs/types/authentication/reducers.type";
+
+export const verifyEmailInitialState: VerifyEmailState = {
+  isFetching: false,
+  statusCode: undefined,
+};
+
+export const verifyEmailReducer = (
+  state: VerifyEmailState,
+  action: VerifyEmailAction
+): VerifyEmailState => {
+  let currentState: VerifyEmailState = { ...state };
+
+  switch (action.type) {
+    case VerifyEmailActionTypes.REQUESTED_VERIFY_EMAIL: {
+      currentState = {
+        isFetching: true,
+        statusCode: undefined,
+      };
+      break;
+    }
+
+    case VerifyEmailActionTypes.RECIVED_VERIFY_EMAIL: {
+      currentState = {
+        isFetching: false,
+        statusCode: (action as RecivedVerifyEmail).statusCode,
+      };
+      break;
+    }
+
+    case VerifyEmailActionTypes.FAILED_VERIFY_EMAIL: {
+      currentState = {
+        isFetching: false,
+        statusCode: (action as FailedVerifyEmail).statusCode,
+      };
+      break;
+    }
+  }
+
+  return currentState;
+};
