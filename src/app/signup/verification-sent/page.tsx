79cb44a301613b4462e7284e608ecc3eaa0350a3
faridEsnaashari <faride13578642@gmail.com>
2023-09-13 13:26:06 +0330

    verification-sent route

---
+++ src/app/signup/verification-sent/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <p>
+      A verification link sent to your email. Please check your email and click
+      on that link to verify your email.
+    </p>
+  );
+}
