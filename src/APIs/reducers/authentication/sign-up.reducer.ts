e2c14bf9d950ff4ab9094aac85766183034216e1
faridEsnaashari <faride13578642@gmail.com>
2023-09-13 19:08:53 +0330

    signUpReducer added

--- src/APIs/reducers/authentication/sign-up.reducer.ts
+++ src/APIs/reducers/authentication/sign-up.reducer.ts
@@ -1,6 +1,15 @@
-import { VerifyEmailAction } from "@/APIs/types/authentication/actions.type";
-import { VerifyEmailActionTypes } from "@/APIs/types/authentication/authentication.enum";
-import { VerifyEmailState } from "@/APIs/types/authentication/reducers.type";
+import {
+  SignUpAction,
+  VerifyEmailAction,
+} from "@/APIs/types/authentication/actions.type";
+import {
+  SignUpActionTypes,
+  VerifyEmailActionTypes,
+} from "@/APIs/types/authentication/authentication.enum";
+import {
+  SignUpState,
+  VerifyEmailState,
+} from "@/APIs/types/authentication/reducers.type";
 
 export const verifyEmailInitialState: VerifyEmailState = {
   isFetching: false,
@@ -9,7 +18,7 @@ export const verifyEmailInitialState: VerifyEmailState = {
 
 export const verifyEmailReducer = (
   state: VerifyEmailState,
-  action: VerifyEmailAction
+  action: VerifyEmailAction,
 ): VerifyEmailState => {
   let currentState: VerifyEmailState = { ...state };
 
@@ -25,7 +34,7 @@ export const verifyEmailReducer = (
     case VerifyEmailActionTypes.RECIVED_VERIFY_EMAIL: {
       currentState = {
         isFetching: false,
-        statusCode: (action as VerifyEmailAction).statusCode,
+        statusCode: action.statusCode,
       };
       break;
     }
@@ -33,7 +42,47 @@ export const verifyEmailReducer = (
     case VerifyEmailActionTypes.FAILED_VERIFY_EMAIL: {
       currentState = {
         isFetching: false,
-        statusCode: (action as VerifyEmailAction).statusCode,
+        statusCode: action.statusCode,
+      };
+      break;
+    }
+  }
+
+  return currentState;
+};
+
+export const signUpReducerInitialState: SignUpState = {
+  isFetching: false,
+  statusCode: undefined,
+};
+
+export const signUpReducer = (
+  state: SignUpState,
+  action: SignUpAction,
+): SignUpState => {
+  let currentState: SignUpState = { ...state };
+
+  switch (action.type) {
+    case SignUpActionTypes.REQUESTED_SIGNUP: {
+      currentState = {
+        isFetching: true,
+        statusCode: undefined,
+      };
+      break;
+    }
+
+    case SignUpActionTypes.RECIVED_SIGNUP: {
+      currentState = {
+        isFetching: false,
+        statusCode: action.statusCode,
+      };
+      break;
+    }
+
+    case SignUpActionTypes.FAILED_SIGNUP: {
+      currentState = {
+        isFetching: false,
+        statusCode: action.statusCode,
       };
       break;
     }
