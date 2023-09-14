7e5c949a6c8cdc1f741ee8c43001398bdfba97d3
faridEsnaashari <faride13578642@gmail.com>
2023-09-14 14:42:54 +0330

    unused data types removed

--- src/APIs/types/authentication/actions.type.ts
+++ src/APIs/types/authentication/actions.type.ts
@@ -5,27 +5,14 @@ import {
   VerifyEmailActionTypes,
 } from "./authentication.enum";
 
-export type VerifyEmailActionData = string;
-
 export type VerifyEmailAction = {
   statusCode?: StatusCodes;
   type: VerifyEmailActionTypes;
 };
 
-export type SignUpActionData = {
-  name: string;
-  email: string;
-  password: string;
-};
-
 export type SignUpAction = {
   statusCode?: StatusCodes;
   type: SignUpActionTypes;
-};
-
-export type LoginActionData = {
-  email: string;
-  password: string;
 };
 
 export type LoginAction = {
