616b1b0fe4bfa14aae74a1f29c3e96b336a4295a
faridEsnaashari <faride13578642@gmail.com>
2023-09-14 14:31:25 +0330

    login action

---
+++ src/APIs/actions/authentication/login.action.ts
@@ -0,0 +1,30 @@
+import {
+  LoginAction,
+  LoginActionData,
+} from "@/APIs/types/authentication/actions.type";
+import { LoginActionTypes } from "@/APIs/types/authentication/authentication.enum";
+import axios from "axios";
+import { Dispatch } from "react";
+
+export const loginAction = (
+  dispatch: Dispatch<LoginAction>,
+  data: LoginActionData
+) => {
+  dispatch({ type: LoginActionTypes.REQUESTD_LOGIN });
+
+  axios
+    .post("/login", data)
+    .then((response) => {
+      dispatch({
+        type: LoginActionTypes.RECIVED_LOGIN,
+        statusCode: response.status,
+        userToken: response?.data?.data?.userToken,
+      });
+    })
+    .catch((error) => {
+      dispatch({
+        type: LoginActionTypes.FAILED_LOGIN,
+        statusCode: error?.response.status,
+      });
+    });
+};
