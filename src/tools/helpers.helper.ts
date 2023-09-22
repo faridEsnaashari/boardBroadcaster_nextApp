5cd666e6dd7690307cd0f1593aeaf29457b59bf5
faridEsnaashari <faride13578642@gmail.com>
2023-09-22 15:12:22 +0330

    getRandomBoardColor function added

--- src/tools/helpers.helper.ts
+++ src/tools/helpers.helper.ts
@@ -1,3 +1,5 @@
+import { BoardColor } from "@/common/types/entities.type";
+
 export const getElementValueById = (id: string): string | undefined => {
   const element = document.getElementById(id);
   if (!element) {
@@ -6,3 +8,14 @@ export const getElementValueById = (id: string): string | undefined => {
 
   return (element as HTMLInputElement).value;
 };
+
+export const getRandomBoardColor = (): BoardColor => {
+  const randomNumber = getRandomNumber(0, 3);
+
+  return Object.values(BoardColor).find(
+    (color, index) => index === randomNumber,
+  )!;
+};
+
+const getRandomNumber = (min: number, max: number): number =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
