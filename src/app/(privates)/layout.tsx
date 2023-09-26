ff55afc946506c49c805694d04cde1774ac1b3d4
faridEsnaashari <faride13578642@gmail.com>
2023-09-26 12:02:07 +0330

    now userData is just the data of the user so don't call userData.data

--- src/app/(privates)/layout.tsx
+++ src/app/(privates)/layout.tsx
@@ -6,7 +6,7 @@ export default async function Layout({ children }: LayoutProps) {
   try {
     const userData = await getUserData(true);
 
-    return <UserContext userData={userData.data}>{children}</UserContext>;
+    return <UserContext userData={userData}>{children}</UserContext>;
   } catch (err) {
     console.error(err);
     throw new Error("you don't have access to this resource");
