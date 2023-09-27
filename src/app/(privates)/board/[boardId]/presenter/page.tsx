90f563b6a6df3040e7fbf4062cae399ea28af604
faridEsnaashari <faride13578642@gmail.com>
2023-09-27 18:15:10 +0330

    DrawingPanel compoennt added. selectedShape isn't null. hoveredShape added. shapes added.

--- src/app/(privates)/board/[boardId]/presenter/page.tsx
+++ src/app/(privates)/board/[boardId]/presenter/page.tsx
@@ -6,6 +6,7 @@ import { UserContext } from "@/contexts/user/user.context";
 import { NotFoundError } from "@/app/errors/not-found.error";
 import styles from "../styles/page.style.module.css";
 import HierarchyPanel from "../components/HierarchyPanel.component";
+import DrawingPanel from "../components/DrawingPanel.component";
 
 export default function Page({ params }: PageParams) {
   const userData = useContext(UserContext);
@@ -18,12 +19,12 @@ export default function Page({ params }: PageParams) {
     return !!board;
   };
 
+  const [shapes, setShapes] = useState<ShapeEntity[]>([]);
   const [boardExists] = useState(doesBoardExist);
   const [shapesListOpening, setShapesListOpening] = useState(false);
   const [actionMode, setActionMode] = useState<ActionMode>(ActionMode.Disable);
-  const [selectedShape, setSelectedShape] = useState<
-    ShapeEntity["name"] | null
-  >(null!);
+  const [selectedShape, setSelectedShape] = useState<ShapeEntity["name"]>("");
+  const [hoveredShape, setHoveredShape] = useState<ShapeEntity["name"]>("");
   const [drawingPanelSize, setDrawingPanelSize] = useState({
     width: 0,
     height: 0,
@@ -41,6 +42,15 @@ export default function Page({ params }: PageParams) {
         setActionMode={setActionMode}
         drawingPanelSize={drawingPanelSize}
         setSelectedShape={setSelectedShape}
+      />
+      <DrawingPanel
+        paintable={true}
+        shapes={shapes}
+        selectedShape={selectedShape}
+        setSelectedShape={setSelectedShape}
+        actionMode={actionMode}
+        hoveredShape={hoveredShape}
+        setDrawingPanelSize={setDrawingPanelSize}
       />
     </div>
   );
