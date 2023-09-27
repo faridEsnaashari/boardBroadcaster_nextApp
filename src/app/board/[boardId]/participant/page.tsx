8ace4c55424bafbe2a597b39744efc637b75d771
faridEsnaashari <faride13578642@gmail.com>
2023-09-27 18:11:39 +0330

    Attributes doesn't provided as ShapeEntity generic.

--- src/app/board/[boardId]/participant/page.tsx
+++ src/app/board/[boardId]/participant/page.tsx
@@ -2,14 +2,11 @@
 
 import DrawingPanel from "@/app/(privates)/board/[boardId]/components/DrawingPanel.component";
 import { useState } from "react";
-import {
-  Attributes,
-  ShapeEntity,
-} from "@/app/(privates)/board/[boardId]/types.type";
+import { ShapeEntity } from "@/app/(privates)/board/[boardId]/types.type";
 import styles from "./styles/page.style.module.css";
 
 export default function Page() {
-  const [shapes, setShapes] = useState<ShapeEntity<Attributes>[]>([]);
+  const [shapes, setShapes] = useState<ShapeEntity[]>([]);
   const [drawingPanelSize, setDrawingPanelSize] = useState({
     width: 0,
     height: 0,
