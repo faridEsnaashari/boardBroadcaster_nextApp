bf5376aeebf53b8d3463741db8e44ca162a1fa78
faridEsnaashari <faride13578642@gmail.com>
2023-09-30 11:32:16 +0330

    useSocket added and the functions needed for socket connection added

--- src/app/(privates)/board/[boardId]/presenter/page.tsx
+++ src/app/(privates)/board/[boardId]/presenter/page.tsx
@@ -7,6 +7,8 @@ import { NotFoundError } from "@/app/errors/not-found.error";
 import styles from "../styles/page.style.module.css";
 import HierarchyPanel from "../components/HierarchyPanel.component";
 import DrawingPanel from "../components/DrawingPanel.component";
+import { useSocket } from "@/hooks/use-socket/use-socket.hook";
+import { changeShapeValues } from "@/tools/helpers.helper";
 
 export default function Page({ params }: PageParams) {
   const userData = useContext(UserContext);
@@ -19,6 +21,20 @@ export default function Page({ params }: PageParams) {
     return !!board;
   };
 
+  const initShapes = (socketShapes: ShapeEntity[]) => {
+    setShapes(
+      socketShapes.map((shape) => ({
+        ...shape,
+        attributes: changeShapeValues("pixel", shape.attributes, {
+          x: drawingPanelSize.width,
+          y: drawingPanelSize.height,
+        }),
+      })),
+    );
+  };
+
+  const socket = useSocket(params.boardId, initShapes);
+
   const [shapes, setShapes] = useState<ShapeEntity[]>([]);
   const [boardExists] = useState(doesBoardExist);
   const [shapesListOpening, setShapesListOpening] = useState(false);
@@ -29,6 +45,108 @@ export default function Page({ params }: PageParams) {
     width: 0,
     height: 0,
   });
+
+  const createNewShape = (newShape: ShapeEntity) => {
+    setShapes([...shapes, newShape]);
+    setSelectedShape(newShape.name);
+
+    console.log(
+      changeShapeValues("relative", newShape.attributes, {
+        x: drawingPanelSize.width,
+        y: drawingPanelSize.height,
+      }),
+    );
+    const socketShape = {
+      ...newShape,
+      attributes: changeShapeValues("relative", newShape.attributes, {
+        x: drawingPanelSize.width,
+        y: drawingPanelSize.height,
+      }),
+    };
+
+    socket.sendShape(socketShape);
+  };
+
+  const updateShape = (updatedShape: ShapeEntity) => {
+    setShapes(
+      shapes.map((shape) =>
+        shape.name === updatedShape.name ? updatedShape : shape,
+      ),
+    );
+
+    const socketShape = {
+      ...updatedShape,
+      attributes: changeShapeValues("relative", updatedShape.attributes, {
+        x: drawingPanelSize.width,
+        y: drawingPanelSize.height,
+      }),
+    };
+
+    socket.sendShapeEventully(socketShape);
+  };
+
+  const onFinishPainting = () => {
+    const shape = shapes.find((shape) => shape.name === selectedShape)!;
+    const socketShape = {
+      ...shape,
+      attributes: changeShapeValues("relative", shape.attributes, {
+        x: drawingPanelSize.width,
+        y: drawingPanelSize.height,
+      }),
+    };
+
+    socket.sendShapeEventully(socketShape);
+  };
+
+  const deleteShape = () => {
+    if (!selectedShape) {
+      return;
+    }
+
+    const deletedShape = shapes.find((shape) => shape.name === selectedShape)!;
+
+    deletedShape.deleted = true;
+
+    setShapes(
+      shapes.map((shape) =>
+        shape.name === deletedShape.name ? deletedShape : shape,
+      ),
+    );
+    setSelectedShape("");
+
+    socket.deleteShape(deletedShape);
+  };
+
+  const deleteAllShapes = () => {
+    const deletedShapes = shapes.map((shape) => ({ ...shape, deleted: true }));
+    setShapes(deletedShapes);
+
+    setSelectedShape("");
+
+    socket.deleteAllShapes(deletedShapes);
+  };
+
+  const duplicateShape = () => {
+    if (!selectedShape) {
+      return;
+    }
+
+    const currentShape = shapes.find((shape) => shape.name === selectedShape);
+
+    const attributes = { ...currentShape!.attributes };
+
+    "x" in attributes && (attributes.x = attributes.x + 10);
+    "y" in attributes && (attributes.y = attributes.y + 10);
+    "x1" in attributes && (attributes.x1 = attributes.x1 + 10);
+    "y1" in attributes && (attributes.y1 = attributes.y1 + 10);
+
+    const name = Date.now() + "";
+    const newShape: ShapeEntity = { ...currentShape!, attributes, name };
+    createNewShape(newShape);
+  };
+
+  const getShowableShapes = (): ShapeEntity[] =>
+    shapes.filter((shape) => !shape.deleted);
 
   if (!boardExists) {
     throw new NotFoundError("board not found");
@@ -41,16 +159,21 @@ export default function Page({ params }: PageParams) {
         actionMode={actionMode}
         setActionMode={setActionMode}
         drawingPanelSize={drawingPanelSize}
-        setSelectedShape={setSelectedShape}
+        onCreateShape={createNewShape}
+        onDeleteShape={deleteShape}
+        onDeleteAllShapes={deleteAllShapes}
+        onDuplicate={duplicateShape}
       />
       <DrawingPanel
         paintable={true}
-        shapes={shapes}
+        shapes={getShowableShapes()}
         selectedShape={selectedShape}
         setSelectedShape={setSelectedShape}
         actionMode={actionMode}
         hoveredShape={hoveredShape}
         setDrawingPanelSize={setDrawingPanelSize}
+        onShapeUpdate={updateShape}
+        onFinishPainting={onFinishPainting}
       />
     </div>
   );
--- src/app/board/[boardId]/participant/page.tsx
+++ src/app/board/[boardId]/participant/page.tsx
@@ -4,18 +4,71 @@ import DrawingPanel from "@/app/(privates)/board/[boardId]/components/DrawingPan
 import { useState } from "react";
 import { ShapeEntity } from "@/app/(privates)/board/[boardId]/types.type";
 import styles from "./styles/page.style.module.css";
+import { changeShapeValues } from "@/tools/helpers.helper";
+import { useSocket } from "@/hooks/use-socket/use-socket.hook";
+import { PageParams } from "./types.type";
 
-export default function Page() {
+export default function Page({ params }: PageParams) {
   const [shapes, setShapes] = useState<ShapeEntity[]>([]);
   const [drawingPanelSize, setDrawingPanelSize] = useState({
     width: 0,
     height: 0,
   });
 
+  const initShapes = (socketShapes: ShapeEntity[]) => {
+    setShapes(
+      socketShapes.map((shape) => ({
+        ...shape,
+        attributes: changeShapeValues("pixel", shape.attributes, {
+          x: drawingPanelSize.width,
+          y: drawingPanelSize.height,
+        }),
+      })),
+    );
+  };
+
+  const onDraw = (updatedShape: ShapeEntity) => {
+    let shapeWasExisted = false;
+
+    const preparedUpdatedShape = {
+      ...updatedShape,
+      attributes: changeShapeValues("pixel", updatedShape.attributes, {
+        x: drawingPanelSize.width,
+        y: drawingPanelSize.height,
+      }),
+    };
+
+    const updatedShapes = shapes.map((shape) => {
+      if (shape.name !== preparedUpdatedShape.name) {
+        return shape;
+      }
+
+      shapeWasExisted = true;
+      return preparedUpdatedShape;
+    });
+
+    shapeWasExisted
+      ? setShapes(updatedShapes)
+      : setShapes([...updatedShapes, preparedUpdatedShape]);
+  };
+
+  const onDelete = (socketShape: ShapeEntity) => {
+    setShapes(
+      shapes.map((shape) =>
+        shape.name === socketShape.name ? socketShape : shape,
+      ),
+    );
+  };
+
+  useSocket(params.boardId, initShapes, onDraw, onDelete);
+
+  const getShowableShapes = (): ShapeEntity[] =>
+    shapes.filter((shape) => !shape.deleted);
+
   return (
     <div className={styles.panelsContainer}>
       <DrawingPanel
-        shapes={shapes}
+        shapes={getShowableShapes()}
         paintable={false}
         setDrawingPanelSize={setDrawingPanelSize}
       />
