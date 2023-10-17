656b319742c697cf435ee817f1585fab93f062c5
faridEsnaashari <faride13578642@gmail.com>
2023-10-17 18:25:09 +0330

    now redirect to login in verification-sent page

--- src/app/signup/verification-sent/page.tsx
+++ src/app/signup/verification-sent/page.tsx
@@ -1,4 +1,13 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Page() {
+  const router = useRouter();
+
+  useEffect(() => router.replace("/login"), []);
+
   return (
     <p>
       A verification link sent to your email. Please check your email and click
